18ead4ff07987c768dde70d3b61e86cb7c3b6cc7
Pat Zabawa <patzabawa@gmail.com>
2016-04-15 19:27:37 -0400

    Obtain reposting_id while crawling first apartment page.

--- craigslist_philadelphia/spiders/test.py
+++ craigslist_philadelphia/spiders/test.py
@@ -9,13 +9,13 @@ class MySpider(Spider):
 
     def parse(self, response):
         hxs = Selector(response)
-        titles = hxs.xpath("//span[@class='pl']")
+        titles = hxs.xpath("//p[@class='row']")
         items = []
         for titles in titles:
             item = CraigslistPhiladelphiaItem()
-            item["id"] = titles.xpath("a/@data-id").extract()
-            item["reposting_id"] = titles.xpath("a/@data-id").extract()
-            item["title"] = titles.xpath("a/span/text()").extract()
+            item["id"] = titles.xpath("@data-pid").extract()
+            item["reposting_id"] = titles.xpath("@data-repost-of").extract()
+            item["title"] = titles.xpath(".//a[@class='hdrlnk']/span/text()").extract()
             item["link"] = titles.xpath("a/@href").extract()
             items.append(item)
         return items
